4a066b9b589bcb08f076bb7b684a4de6b08011e6
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-07 00:58:51 +0600

    form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(name, email, password);
+    }
+
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
 
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <form
+                            onSubmit={handleSubmit}
+                            className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="name" className="input input-bordered" />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -19,14 +40,11 @@ const Login = () => {
                                     <span className="label-text">Password</span>
                                 </label>
                                 <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <input className="btn btn-success" type="submit" value="Login" />
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
